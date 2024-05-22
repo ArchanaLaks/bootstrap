c761eb4be2ea4586aec0b19dddf08c5c8f977c8e
ArchanaLaks <archanaraju122@gmail.com>
2024-05-22 11:04:12 +0530

    sliders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,6 @@ import { ImagerComponent } from './imager/imager.component';
 import { SliderComponent } from './slider/slider.component';
 import { AnimationComponent } from './animation/animation.component';
 import { Animation2Component } from './animation2/animation2.component';
-import { Animation3Component } from './animation3/animation3.component';
-import { Animation4Component } from './animation4/animation4.component';
-import { Animation5Component } from './animation5/animation5.component';
-import { Animation6Component } from './animation6/animation6.component';
-import { Animation7Component } from './animation7/animation7.component';
-import { Animation8Component } from './animation8/animation8.component';
 
 @NgModule({
   declarations: [
@@ -25,12 +19,6 @@ import { Animation8Component } from './animation8/animation8.component';
     SliderComponent,
     AnimationComponent,
     Animation2Component,
-    Animation3Component,
-    Animation4Component,
-    Animation5Component,
-    Animation6Component,
-    Animation7Component,
-    Animation8Component,
   ],
   imports: [
     BrowserModule,
